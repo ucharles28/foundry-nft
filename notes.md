ca509d49ee11fa98b707e8f870c1112662f29709
ucharles28 <uzomacharles28@gmail.com>
2024-08-14 07:35:14 +0100

    add files

---
+++ notes.md
@@ -0,0 +1,7 @@
+# Get function selector
+- cast sig "transfer(address,address,uint256)"
+
+- openchain.xyz : check function selector
+
+# Decode function data 
+- cast --calldata-decode "transferFrom(address,address,uint256)" CALL_DATA
---
+++ script/Interactions.s.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+
+import {Script} from "forge-std/Script.sol";
+import {console} from "forge-std/console.sol";
+import {DevOpsTools} from "foundry-devops/src/DevOpsTools.sol";
+import {BasicNft} from "../src/BasicNft.sol";
+
+contract MintBasicNft is Script {
+    string public constant PUG_URI =
+        "ipfs://bafybeig37ioir76s7mg5oobetncojcm3c3hxasyd4rvid4jqhy4gkaheg4/?filename=0-PUG.json";
+
+    function run() external {
+        address mostRecentlyDeployedBasicNft = DevOpsTools.get_most_recent_deployment("BasicNft", block.chainid);
+        mintNftOnContract(mostRecentlyDeployedBasicNft);
+    }
+
+    function mintNftOnContract(address basicNftAddress) public {
+        vm.startBroadcast();
+        BasicNft(basicNftAddress).mintNft(PUG_URI);
+        vm.stopBroadcast();
+    }
+}
